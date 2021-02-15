ec9a7570d614fc9e86860b9ad7e11d59471a1d78
Gunjan Raj Tiwari <im.gunjan1@gmail.com>
2021-02-15 19:18:39 +0545

    made responsive

--- scripts.js
+++ scripts.js
@@ -4,6 +4,7 @@ new Glider(document.querySelector('.glider'), {
   slidesToScroll: 1,
   scrollLock: true,
   draggable: true,
+  duration: 0.5,
   dots: '#dots',
   arrows: {
     prev: '.glider-prev',
@@ -12,22 +13,22 @@ new Glider(document.querySelector('.glider'), {
   responsive: [
     {
       // screens greater than >= 775px
-      breakpoint: 775,
+      breakpoint: 1080,
       settings: {
         // Set to `auto` and provide item width to adjust to viewport
-        slidesToShow: 'auto',
+        slidesToShow: 3,
         slidesToScroll: 'auto',
-        itemWidth: 150,
-        duration: 0.25
+        duration: 0.5
       }
-    },{
-      // screens greater than >= 1024px
-      breakpoint: 1024,
+    },
+    {
+      // screens greater than >= 775px
+      breakpoint: 720,
       settings: {
+        // Set to `auto` and provide item width to adjust to viewport
         slidesToShow: 2,
-        slidesToScroll: 1,
-        itemWidth: 150,
-        duration: 0.25
+        slidesToScroll: 'auto',
+        duration: 0.5
       }
     }
   ]
